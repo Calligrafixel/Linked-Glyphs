6f8685e715dd5a1baa08dda84ff5fdbd6a42aa30
Calligrafixel <47078648+Calligrafixel@users.noreply.github.com>
2022-11-27 15:39:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const _preview = document.getElementById("preview");
 _preview.focus();
+
+
+const _info = document.getElementById("info_popup");
+const _process = document.getElementById("process_popup");
+
+_preview.addEventListener("keypress", (e) => {
+    const k = e.keyCode;
+
+    if (k == 49) {
+        _info.style.display = "block";
+        _process.style.display = "none";
+    }
+    else if (k == 50) {
+        _info.style.display = "none";
+        _process.style.display = "block";
+    } else if (k == 51) {
+        _info.style.display = "none";
+        _process.style.display = "none";
+    }
+});
